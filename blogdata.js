e0c735c72af09ee0521def67063a631539b69aae
Penny20160 <ajg.sikma@gmail.com>
2025-02-15 10:54:26 +0100

    Update blogdata.js

--- blogdata.js
+++ blogdata.js
@@ -24,31 +24,31 @@ const data = [
                     "text": "Vandaag stond ik met Lila en Ruud Geers bij een gallerie in Den Haag. Lila en ik hebben het plafond gerepareerd terwijl Ruud het plafond in de andere kamer schilderde. Daarna hebben Lila en ik die kamer opgeruimd. En na de lunch die andere kamer."
                 }]
             }
-        ]
+    
     },
     {
         "label": "De tweede week",
         "dagen": [
             {
                 "datum": "maandag 10-02-2025",
-                "entries": [{ "text": "Om half 6 opgestaan om op tijd klaar te zijn. Ik had van te voren gevraagd hoe laat ik waar moest zijn.             Dat was om half 8 bij station leidschendam voorburg. Ik was om er om kwart over 7, ruim op tijd dus. Ik werd ietjes later opgehaald, omdat er een vrachtwagen             vast kwam te zitten onder het station. Eenmaal aangekomen gingen we eerst koffie drinken en kennis maken met de rest van het personeel. ik kreeg toen ook te horen             dat ik deze dag op de werkplaats zou blijven met Nadine. Ik ging de luiken/deuren(die dingen waren zowat net zo groot als ik) schuren. Ik heb er uiteindelijk 7,5 van af gekregen na een hele dag schuren. Daarna ging ik naar huis" }]
+                "entries": [{ "text": "Ik stond vandaag met Anneke en Lila op de werkplaats. Ik stond boven met Anneke de luiken te schuren en te reparen, terwijl Lila de laatste luiken grondde. Het ging volgens mij best oke." }]
             },
             {
                 "datum": "dinsdag 11-02-2025",
                 "entries": [{
-                    "text": "ik mocht vandaag met Timo mee, naar Leiden. Dinsdag stond ik ook met Nadine, maar toen heb ik de deuren gegrond terwijl Nadine schuurde. Ik had overigens wel gezegd dat we konden afwisselen. Ik mocht ook wat later beginnen, maar dat was wat laat aangegeven waardoor ik s'ochtends wat te vroeg was opgestaan. Ik heb vandaag de muren en plafond geschilderd. De muren met de kwast en roller, maar het plafond alleen met de roller. verder heb ik een zijkant van een kast gerepareerd en de plinten geschuurd. Het was heel gezellig met Timo en Jasmijn, maar we hebben ook hard gewerkt. Morgen sta ik weer op de werkplaats."
+                    "text": "Ik stond vandaag weer met Anneke en Lila op de werkplaats. De rest van de luiken repareren. We stonden nu wel met z'n drieën boven waar het een stukje warmer is. Aan het eind van de middag werden we gebeld dat ik morgen vrij was.Donderdag sta ik met Anneke op de werkplaats."
                 }]
             },
             {
                 "datum": "donderdag 13-02-2025",
                 "entries": [{
-                    "text": "alle luiken aan de ene kant gegrond en de helft beide kanten. "
+                    "text": "Het plan was gewijzigd waardoor ik nu met Timo, Anneke en Jasmijn op de klus in Leiden stond. Ik heb vandaag een deur beneden gegrond. Het was er een met veel besnijdwerk. Het ging niet zo heel goed. Daarna ging ik stofzuigen. Er was overigens wel iets mis met de verf, er zat geen pigment in. Daarom moeten Timo en ik morgen terug naar Leiden. De planning is ook dat we Ruud's klus afmaken. Morgen word het dus druk. "
                 }]
             },
             {
                 "datum": "vrijdag 14-02-2025",
                 "entries": [{
-                    "text": "Vandaag stond ik met Lila en Ruud Geers bij een gallerie in Den Haag. Lila en ik hebben het plafond gerepareerd terwijl Ruud het plafond in de andere kamer schilderde. Daarna hebben Lila en ik die kamer opgeruimd. En na de lunch die andere kamer."
+                    "text": "We hebben niet beide klussen afgekregen, daarvoor waren we te laat klaar in Leiden. Ik heb eerst s'ochtends het mislukte besnijdwerk weg gewerkt met een plammuurmesje. Het glasmesje hadden we per ongeluk gisteren al opgeruimd. Ik heb per ongeluk wel een ruitje gebroken tijdens het krabben. Timo heeft me daarna wel gelijk een tip gegeven hoe ik dat kon voorkomen. Daarna heb ik de spullen die we niet meer nodig hadden opgeruimd. Ook heb ik nog een klein deel van het plafond geschilderd met de kwast. Daarna heb ik de laatste dingen opgeruimd en zijn we naar huis gegaan."
                 }]
             }
         ]

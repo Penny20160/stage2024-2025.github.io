18d98ac0381c49c3585b5c287cf9db52e60ca120
Penny20160 <ajg.sikma@gmail.com>
2025-04-05 10:53:53 +0200

    Update blogdata.js

--- blogdata.js
+++ blogdata.js
@@ -81,38 +81,6 @@ const data = [
                     "text": " Ik heb vandaag gesprek gehad met Ruud en Jos. Wat daar is uitgekomen is rot"
                 
                     }]
-            }]},
-    {
-    "label": "week 5",    
-    "dagen": [
-            {
-                "datum": "maandag 03-03-2025",
-                "entries": [{ "text": "ik ben vandaag begonnen bij het project in Utrecht. ik sta in het groepje van de kantine. ik heb de pilaren voorbereid en geschilderd"
-            }]
-            },
-            {
-                "datum": "woensdag 05-03-2025",
-                "entries":[{
-                    "text": " Ik heb de muren donkergroen geschilderd. het besnijden ging bij de tweede muur niet helemaal goed, maar dat werk ik maandag wel weg. ook heb ik de laatste twee plinten gegrond."
-                
-                    }]
-            }]
-    },
-    {
-    "label": "week 6",    
-    "dagen": [
-            {
-                "datum": "maandag 10-03-2025",
-                "entries": [{ "text": "Ik heb vandaag de uitschieters weggewerkt. "
-            }]
-            },
-            {
-                "datum": "woensdag 12-03-2025",
-                "entries":[{
-                    "text": " Ik heb vandaag de plinten allemaal de tweede laag gegeven. Daarna heb ik de tape weggehaald bij de deur en de plinten. De rest van de dag heb ik Sam geholpen met de voorbereiding van de decoratie."
-                
-                    }]
-            },
-        ]
-    }];  
+            }]}];
+     
     

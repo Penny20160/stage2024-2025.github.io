545c1ef8994d2ad928e572c3ac18235dd45f094a
Penny20160 <ajg.sikma@gmail.com>
2025-04-05 12:08:07 +0200

    Update blogdata1.js

--- blogdata1.js
+++ blogdata1.js
@@ -25,9 +25,39 @@ const data [ {
             {
                 "datum": "woensdag 12-03-2025",
                 "entries":[{
-                    "text": " Ik heb vandaag de plinten allemaal de tweede laag gegeven. Daarna heb ik de tape weggehaald bij de deur en de plinten. De rest van de dag heb ik Sam geholpen met de voorbereiding van de decoratie."
+                    "text": " Ik heb vandaag de plinten allemaal de tweede laag gegeven. Daarna heb ik de tape weggehaald bij de deur en de plinten. De rest van de dag heb ik Sam geholpen met de voorbereiding van de decoratie. oftewel het aftapen van het voetbalstadion van Utrecht.ik geef niks om voetbal"
                 
                     }]
             },
         ]
-    }];
+    },
+            {
+                "label":"week 7",
+                "dagen":[
+                "entries":[
+                    {
+                     "text":"deze week heb ik gewerkt aan het uittekenen van de dom. Het is uiteindelijk niet zo heel goed gegaan want de proporties kloppen niet helemaal. Dit ga ik volgende week verbeteren.  " 
+                    }]]
+            },
+            {
+                "label":"week 8",
+                "dagen":[
+                "entries":[
+                    {
+                        "text":"Bij de dom was het helemaal fout gegaan. Daardoor kon ik op een gegeven moment alles niet meer zo goed hebben. Daarom ging ik op dinsdag aan de kleurenstaat werken. Dat ging me wel goed af. Op woensdag ben ik uiteindelijk op het bijwerken gezet van koffievlekken."
+                    }
+                    ]]
+                    
+            },
+            {
+                "label":"week 9",
+                "dagen":[
+                "entries":[
+                    {
+                      "text":"maandag was de laatste dag bij een locatie van de Gemeente Utrecht. Ik heb de laatste bijwerkingen van onze ruimten gedaan en de kleuren apart gezet. Dinsdag begonnen we op een nieuwe locatie en dat betekent schoonmaken. Na het schoonmaken op dinsdag begon ik met schuren en daarna hebben we de muren en radiatoren voor het eerst gegrond. Het besnijden hierbij ging best goed. 
+                    }
+                ]
+            ]
+                    }
+                
+    ];

02a0561ccca45f67837eeecd7663e76e4fcaa6dc
Penny20160 <ajg.sikma@gmail.com>
2025-04-06 10:25:22 +0200

    Update blogdata1.js

--- blogdata1.js
+++ blogdata1.js
@@ -33,30 +33,31 @@ const data =[ {
     },
             {
                 "label":"week 7",
-                "dagen":[
+                "dagen":[{
                 "entries":[
                     {
                      "text":"deze week heb ik gewerkt aan het uittekenen van de dom. Het is uiteindelijk niet zo heel goed gegaan want de proporties kloppen niet helemaal. Dit ga ik volgende week verbeteren.  " 
-                    }]]
+                    }]}]
             },
             {
                 "label":"week 8",
-                "dagen":[
+                "dagen":[{
                 "entries":[
                     {
                         "text":"Bij de dom was het helemaal fout gegaan. Daardoor kon ik op een gegeven moment alles niet meer zo goed hebben. Daarom ging ik op dinsdag aan de kleurenstaat werken. Dat ging me wel goed af. Op woensdag ben ik uiteindelijk op het bijwerken gezet van koffievlekken."
                     }
-                    ]]
+                    ]}]
                     
             },
             {
                 "label":"week 9",
-                "dagen":[
+                "dagen":[{
                 "entries":[
                     {
                       "text":"maandag was de laatste dag bij een locatie van de Gemeente Utrecht. Ik heb de laatste bijwerkingen van onze ruimten gedaan en de kleuren apart gezet. Dinsdag begonnen we op een nieuwe locatie en dat betekent schoonmaken. Na het schoonmaken op dinsdag begon ik met schuren en daarna hebben we de muren en radiatoren voor het eerst gegrond. Het besnijden hierbij ging best goed. 
                     }
                 ]
+                }
             ]
                     }
                 

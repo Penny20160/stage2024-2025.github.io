7bf5c2816ab3aab7c988f4d46ebf784a01b5bb9b
Penny20160 <ajg.sikma@gmail.com>
2025-04-26 10:15:44 +0200

    Update blogdata2.js

--- blogdata2.js
+++ blogdata2.js
@@ -1,1 +1,34 @@
-
+const data =[ {
+    "label": "week 11",    
+    "dagen": [
+            {
+                "datum": "maandag 14-04-2025",
+                "entries": [{ "text": "Ik ben vandaag begonnen bij Koek en Reinders. Ik begon s'ochtends met Jos m'n begeleider.  We gingen naar de Gevangenpoort voor het opmeten van het glas. Ik heb uiteindelijk de maten opgeschreven. Daarna kwam er iemand voor het verftechnisch advies. Die deed een hechtingsproefje en deed daar een beetje officieel over, omdat er een stagiare bij stond. Toen we weer terug waren heb ik de maten uitgewerkt in een excel bestand. Vandaag heb ik moeten lopen vanaf het station, maar vanaf morgen wordt ik opgehaald. "
+            }]
+            },
+            {
+                "datum": "dinsdag 15-04-2025",
+                "entries":[{
+                    "text": "Jos kreeg deze ochtend een klus waar hij geen afleiding bij kon gebruiken dus ik heb vandaag Patrick geholpen. Ik heb een beetje de werkplaats opgeruimd en 2 bestanden met meetstaten gemaakt. 1 van het stadhuis van Gouda en de ander weet ik niet meer precies waarvan. De rest van de dag heb ik mezelf een beetje vermaakt."
+                
+                    }]
+            },
+           {
+             "datum":"woensdag 16-04-2025",
+             "entries":[{
+               "text" :" Deze ochtend begon ik met het uitypen van de maten voor de ramen en kozijnen van het Huygens museum; een museum waar ik blijkbaar elke dag langs kom. Dat was voor een offerte. Daarna ben ik met Kees naar het paviljoen op scheveningen en het orangjehotel geweest. Daarna heb ik hem geholpen met het maken van een offerte, ik weet niet meer precies waarvoor. s'ochtends was al bekend wat ik morgen ga doen, namelijk letters zetten met Angelo in Rotterdam." 
+             }]
+           },
+           {
+             "datum":"donderdag 17-04-2025",
+             "entries":[{
+               "text" :" Zoals gisteren bekend was gemaakt ging ik met Angelo naar Rotterdam. We begonnen s'ochtends met het opmeten van waar de letters moesten komen, want de mensen die op die klus stonden hadden ze niet opgemeten. We hadden wel een stel geprinte letters meegekregen, maar het bleek dat de mensen die die letters geprint hadden ze niet op gelijke hoogte hadden gemaakt. Angelo heeft die uiteindelijk aangepast. De letters moesten in 1 keer goed dus de omtrek deed Angelo en ik mocht ze inkleuren. Dat ging redelijk alhoewel ik er wel een aantal keer naast ben gegaan. We waren rond 2 uur klaar dus we zijn eerst nog naar kantoor gereden. Nadat we de bus hadden uitgeruimd, mochten we naar huis. Was mijn moeder best blij mee, kon ik nog helpen met de vouwwagen."
+             }]
+           }]
+           {   
+               "label":"week 12",
+               "dagen":[{
+                 
+               }]
+             }]
+             

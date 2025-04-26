1c5288972b2d880e84f71c59b149bf4152efef92
Penny20160 <ajg.sikma@gmail.com>
2025-04-26 10:57:25 +0200

    Update blogdata2.js

--- blogdata2.js
+++ blogdata2.js
@@ -25,10 +25,31 @@ const data =[ {
                "text" :" Zoals gisteren bekend was gemaakt ging ik met Angelo naar Rotterdam. We begonnen s'ochtends met het opmeten van waar de letters moesten komen, want de mensen die op die klus stonden hadden ze niet opgemeten. We hadden wel een stel geprinte letters meegekregen, maar het bleek dat de mensen die die letters geprint hadden ze niet op gelijke hoogte hadden gemaakt. Angelo heeft die uiteindelijk aangepast. De letters moesten in 1 keer goed dus de omtrek deed Angelo en ik mocht ze inkleuren. Dat ging redelijk alhoewel ik er wel een aantal keer naast ben gegaan. We waren rond 2 uur klaar dus we zijn eerst nog naar kantoor gereden. Nadat we de bus hadden uitgeruimd, mochten we naar huis. Was mijn moeder best blij mee, kon ik nog helpen met de vouwwagen."
              }]
            }]
+           },
            {   
                "label":"week 12",
                "dagen":[{
-                 
-               }]
+                 "datum":"dinsdag 22-04-2025",
+                   "entries":[{
+                       "text":"Ik heb vandaag Danny een beetje geholpen. Hij had een stel tekeningen meegekregen waaruit hij de maten van de ruiten moest halen. Hij deed dat en ik typte mee. Daarna heb ik wat stickers van Koek en reinders opgeplakt op de nieuwe steigers, zodat ze niet gestolen worden. Verder ben ik nog met Kees meegeweest en daarna heb ik een beetje meegekeken met wat Danny aan het doen was voor een offerte."
+                   }]
+               },{
+                   "datum":"woensdag 23-04-2025",
+                   "entries":[{
+                       "text":"Vandaag heb ik Kees geholpen met het indexeren van een Offerte. Indexeren betekent de prijs verhogen van een offerte volgens een vastgesteld aantal procenten. Dit ging prima. Helaas was het bestand daarvoor al afgesloten, wat betekende dat ik de uren een beetje op de gok moest verhogen tot het bedrag klopte. Ik deed wel de verhoudingsberekingen maar die klopte niet helemaal dus het was een beetje een iritant klusje. Dat koste me het meeste van de dag. Daarna heb ik een beetje meegekeken.  "
+                   }]
+               },
+                {
+                   "datum":"donderdag 24-04-2025",
+                    "entries":[{
+                        "text":"Vandaag heb ik een klusje voor Team Glas in het algemeen gedaan. Ik moest een overzicht maken van alle glasklussen op naam, nummer,acceptatiedatum, bestellingsdatum, leveringsdatum en aanneemsom. Tenminste degenen die ze op papier hadden staan, het zijn er waarschijnlijk meer. Ze kunnen niet zo goed orde houden. Ik ben dus de hele dag bezig geweest met de mails van Willem en Danny doorspitten op de computer van Willem, omdat ik zelf geen toegang tot de server heb. ik heb vrijwel alles gedaan behalve de laatste 4, maar die doe ik morgen. Ik moet vanaf morgen ook de vissen in het aquarium voeren, want Willem is vanaf morgen op vakantie.
+                    }]
+                 },
+                    {
+                     "datum":"vrijdag 25-04-2025",
+                        "entries":[{
+                         "text":"Vandaag heb ik het grootste deel van dag met Friso opgetrokken. Eerst heb ik natuurlijk het overzicht afgemaakt en de visjes eten gegeven. Daarna ben ik na de pause met Friso meegeweest naar een woonhuis op scheveningen om te melden dat ze daar maandag gingen beginnen. S'middags hebben we de voorbereidingen getroffen voor een bezoek aan het stadhuis van Gouda. Dit betekende het bestek lezen en wat achtergrondinformatie opzoeken. Daarna heb ik een offerte aangepast, zonder uitleg dus ik mocht het zelf uitzoeken. We konden het alleen niet helemaal checken omdat Friso in gesprek was tot 10 minuten voor we gingen stoppen.    
+                        }]
+                     }]
              }]
              

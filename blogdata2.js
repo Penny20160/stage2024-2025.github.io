ce719e67bce3b5b2bd8bf276d59af48b21ca955c
Alys001 <129740016+Alys001@users.noreply.github.com>
2025-05-11 07:22:12 +0200

    blog aangepast en foto's toegevoegd.

--- blogdata2.js
+++ blogdata2.js
@@ -1,30 +1,54 @@
 const data =[ {
     "label": "week 11",    
     "dagen": [
-            {
-                "datum": "maandag 14-04-2025",
-                "entries": [{ "text": "Ik ben vandaag begonnen bij Koek en Reinders. Ik begon s'ochtends met Jos m'n begeleider.  We gingen naar de Gevangenpoort voor het opmeten van het glas. Ik heb uiteindelijk de maten opgeschreven. Daarna kwam er iemand voor het verftechnisch advies. Die deed een hechtingsproefje en deed daar een beetje officieel over, omdat er een stagiare bij stond. Toen we weer terug waren heb ik de maten uitgewerkt in een excel bestand. Vandaag heb ik moeten lopen vanaf het station, maar vanaf morgen wordt ik opgehaald. "
+        {
+            "datum": "maandag 14-04-2025",
+            "entries": [{
+                "text": "Ik ben vandaag begonnen bij Koek en Reinders. Ik begon s'ochtends met Jos m'n begeleider.  We gingen naar de Gevangenpoort voor het opmeten van het glas. Ik heb uiteindelijk de maten opgeschreven. Daarna kwam er iemand voor het verftechnisch advies. Die deed een hechtingsproefje en deed daar een beetje officieel over, omdat er een stagiare bij stond. Toen we weer terug waren heb ik de maten uitgewerkt in een excel bestand. Vandaag heb ik moeten lopen vanaf het station, maar vanaf morgen wordt ik opgehaald. "
             }]
-            },
-            {
-                "datum": "dinsdag 15-04-2025",
-                "entries":[{
-                    "text": "Jos kreeg deze ochtend een klus waar hij geen afleiding bij kon gebruiken dus ik heb vandaag Patrick geholpen. Ik heb een beetje de werkplaats opgeruimd en 2 bestanden met meetstaten gemaakt. 1 van het stadhuis van Gouda en de ander weet ik niet meer precies waarvan. De rest van de dag heb ik mezelf een beetje vermaakt."
-                
-                    }]
-            },
-           {
-             "datum":"woensdag 16-04-2025",
-             "entries":[{
-               "text" :" Deze ochtend begon ik met het uitypen van de maten voor de ramen en kozijnen van het Huygens museum; een museum waar ik blijkbaar elke dag langs kom. Dat was voor een offerte. Daarna ben ik met Kees naar het paviljoen op scheveningen en het orangjehotel geweest. Daarna heb ik hem geholpen met het maken van een offerte, ik weet niet meer precies waarvoor. s'ochtends was al bekend wat ik morgen ga doen, namelijk letters zetten met Angelo in Rotterdam." 
-             }]
-           },
-           {
-             "datum":"donderdag 17-04-2025",
-             "entries":[{
-               "text" :" Zoals gisteren bekend was gemaakt ging ik met Angelo naar Rotterdam. We begonnen s'ochtends met het opmeten van waar de letters moesten komen, want de mensen die op die klus stonden hadden ze niet opgemeten. We hadden wel een stel geprinte letters meegekregen, maar het bleek dat de mensen die die letters geprint hadden ze niet op gelijke hoogte hadden gemaakt. Angelo heeft die uiteindelijk aangepast. De letters moesten in 1 keer goed dus de omtrek deed Angelo en ik mocht ze inkleuren. Dat ging redelijk alhoewel ik er wel een aantal keer naast ben gegaan. We waren rond 2 uur klaar dus we zijn eerst nog naar kantoor gereden. Nadat we de bus hadden uitgeruimd, mochten we naar huis. Was mijn moeder best blij mee, kon ik nog helpen met de vouwwagen."
-             }]
-           }]
+        },
+        {
+            "datum": "dinsdag 15-04-2025",
+            "entries": [{
+                "text": "Jos kreeg deze ochtend een klus waar hij geen afleiding bij kon gebruiken dus ik heb vandaag Patrick geholpen. Ik heb een beetje de werkplaats opgeruimd en 2 bestanden met meetstaten gemaakt. 1 van het stadhuis van Gouda en de ander weet ik niet meer precies waarvan. De rest van de dag heb ik mezelf een beetje vermaakt."
+
+            }]
+        },
+        {
+            "datum": "woensdag 16-04-2025",
+            "entries": [{
+                "text": " Deze ochtend begon ik met het uitypen van de maten voor de ramen en kozijnen van het Huygens museum; een museum waar ik blijkbaar elke dag langs kom. Dat was voor een offerte. Daarna ben ik met Kees naar het paviljoen op scheveningen en het orangjehotel geweest. Daarna heb ik hem geholpen met het maken van een offerte, ik weet niet meer precies waarvoor. s'ochtends was al bekend wat ik morgen ga doen, namelijk letters zetten met Angelo in Rotterdam."
+            }]
+        },
+        {
+            "datum": "donderdag 17-04-2025",
+            "entries": [{
+                "text": " Zoals gisteren bekend was gemaakt ging ik met Angelo naar Rotterdam. We begonnen s'ochtends met het opmeten van waar de letters moesten komen, want de mensen die op die klus stonden hadden ze niet opgemeten. We hadden wel een stel geprinte letters meegekregen, maar het bleek dat de mensen die die letters geprint hadden ze niet op gelijke hoogte hadden gemaakt. Angelo heeft die uiteindelijk aangepast. De letters moesten in 1 keer goed dus de omtrek deed Angelo en ik mocht ze inkleuren. Dat ging redelijk alhoewel ik er wel een aantal keer naast ben gegaan. We waren rond 2 uur klaar dus we zijn eerst nog naar kantoor gereden. Nadat we de bus hadden uitgeruimd, mochten we naar huis. Was mijn moeder best blij mee, kon ik nog helpen met de vouwwagen."
+            }]
+        }],
+    "fotos": [
+        {
+            "src": "Image(9).jpg"
+        },
+        {
+            "src": "Image(10).jpg"
+        },
+        {
+            "src": "Image(11).jpg"
+        },
+        {
+            "src": "Image(12).jpg"
+        },
+        {
+            "src": "Image(13).jpg"
+        },
+        {
+            "src": "Image(14).jpg"
+        },
+        {
+            "src": "Image(15).jpg"
+        }]
+
            },
            {   
                "label":"week 12",
@@ -50,7 +74,13 @@ const data =[ {
                         "entries":[{
                          "text":"Vandaag heb ik het grootste deel van dag met Friso opgetrokken. Eerst heb ik natuurlijk het overzicht afgemaakt en de visjes eten gegeven. Daarna ben ik na de pause met Friso meegeweest naar een woonhuis op scheveningen om te melden dat ze daar maandag gingen beginnen. S'middags hebben we de voorbereidingen getroffen voor een bezoek aan het stadhuis van Gouda. Dit betekende het bestek lezen en wat achtergrondinformatie opzoeken. Daarna heb ik een offerte aangepast, zonder uitleg dus ik mocht het zelf uitzoeken. We konden het alleen niet helemaal checken omdat Friso in gesprek was tot 10 minuten voor we gingen stoppen.  "  
                         }]
-                     }]
+                   }
+               ],
+               "fotos": [
+                   {
+                       "src": "Image(18).jpg"
+                   }
+               ]
              },
              {
                  "label":"week 13",
@@ -101,9 +131,14 @@ const data =[ {
                  },{
                  "datum":"donderdag 08-05-2025",
                  "entries":[{
-                     "text":"Deze ochtend heb ik eerst aan mijn stageverslag gewerkt terwijl Jos mijn planning aan het verbeteren is."
+                     "text":"Deze ochtend heb ik eerst aan mijn stageverslag gewerkt terwijl Jos mijn planning aan het verbeteren is. Daarna heb ik de uren van de planning gesplitst omdat die bij sommige gevels bij een andere fase hoorde. Toen ik klaar was was het al weer tijd om naar huis te gaan."
                  }]
-             }],
+                 }, {
+                     "datum": "vrijdag 09-05-2025",
+                     "entries": [{
+                         "text":"Ik heb vandaag de letters overgetrokken bij het hofje van Nieuwkoop. Dat ging best oke. We mochten als we eerder klaar waren eerder weg dus dat hebben we ook gedaan. Ik heb thuis toen dus nog even van het zonnetje genoten. "
+                     }]
+                 }],
             "fotos":[
                 {
                     "src":"IMG_2522.jpg"
@@ -128,6 +163,18 @@ const data =[ {
                 {
                     "src":"IMG_2535.jpg"
 
+                },
+                {
+                    "src":"Image(7).jpg"
+                },
+                {
+                    "src":"Image(8).jpg"
+                },
+                {
+                    "src":"Image(16).jpg"
+                },
+                {
+                    "src":"Image(17).jpg"
                 }
             ]
              }]
